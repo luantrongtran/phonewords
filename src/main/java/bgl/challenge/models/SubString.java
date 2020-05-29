f5b1202fefa3d45bf8c022a1106440e47f0faf70
luant <luanlua12388@gmail.com>
2020-05-29 14:26:31 +1000

    Adding equal & hashCode for SubString

--- src/main/java/bgl/challenge/models/SubString.java
+++ src/main/java/bgl/challenge/models/SubString.java
@@ -77,4 +77,41 @@ public class SubString {
 	public void setEnd(int end) {
 		this.end = end;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + end;
+		result = prime * result + ((originalString == null) ? 0 : originalString.hashCode());
+		result = prime * result + start;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SubString other = (SubString) obj;
+		if (end != other.end)
+			return false;
+		if (originalString == null) {
+			if (other.originalString != null)
+				return false;
+		} else if (!originalString.equals(other.originalString))
+			return false;
+		if (start != other.start)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
